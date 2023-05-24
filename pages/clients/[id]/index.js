cbcc88ba8485ed5647030bf8d734133ba2cfbf94
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-05-24 16:16:55 +0300

    save

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const ClientProjectsPage = () => {
+  const router = useRouter();
+
+const clientPojectId = () => {
+router.replace('/clients/[id]/[clientsprojectid]')
+}
   return(
     <div>
       <h1>The Project of a Given Client</h1>
+      <button onClick={clientPojectId}>Click</button>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+
 const HomePage = () => {
+  const arrClient = [
+    {id: "max", name: "maxim"},
+    {id: "dima", name: "dima"}
+  ]
+
   return (
     <>
     <h1>Hello Nextjs hhhh</h1>
+    <ul>
+    {
+      arrClient.map(({id, name}) => {
+        return <li key={id}><Link href={`/clients/${id}`}>{name}</Link></li>
+      })
+     }
+    </ul>
     </>
   )
 }
